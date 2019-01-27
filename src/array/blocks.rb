bdb45131b9e0d28c802d6842e2d3c422300333d6
Gregory K. Spranger <greg@spranger.us>
2019-01-27 11:28:57 -0500

    more fun

---
+++ src/array/blocks.rb
@@ -0,0 +1,9 @@
+arr = [1,3,4,5,3,3,5,3,3]
+
+arr.each { |e| p e }
+
+arr.each do |e|
+  p e
+end
+
+10.times { |number| p "count #{number}" }
---
+++ src/array/each.rb
@@ -0,0 +1,5 @@
+arr = [1,2,4,5,6,6,5,4,3]
+
+arr.each do |e|
+  p e
+end
